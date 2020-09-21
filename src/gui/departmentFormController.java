38f3f0e06d5c22f378c60b71165017dc3fa57d3c
Vinicius Vieira Carneiro <vinicius.95vieira00@gmail.com>
2020-09-21 15:40:54 -0300

    Saving a new Department

--- src/gui/departmentFormController.java
+++ src/gui/departmentFormController.java
@@ -3,18 +3,25 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
 import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class departmentFormController implements Initializable {
 
 	private Department entity;
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TextField textId;
@@ -35,14 +42,41 @@ public class departmentFormController implements Initializable {
 		this.entity = entity;
 	}
 	
-	@FXML
-	public void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@FXML
-	public void onBTCancelAction() {
-		System.out.println("onBtCancelAction");
+	public void onBtSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+		entity = getFormData();
+		service.saveOrUpdate(entity);
+		
+		Utils.currentStage(event).close();
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+		}
+	}
+	
+	private Department getFormData(){
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryPArsetoInt( textId.getText()));
+		obj.setName(textName.getText());
+		
+		return obj;
+	}
+	
+	@FXML
+	public void onBTCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
